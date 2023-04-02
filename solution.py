2b106e1e83a5be5e6170683c104e3033de969268
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-02 21:26:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,10 +13,11 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     n = len(x)
     s_squared = np.var(x, ddof=1)
+    disp_coef = 23 * (2 - np.pi / 2)
     
     nom = (n - 1) * s_squared
-    rv = chi2(df = 2 * (n - 1))
-    left = rv.ppf(1 - alpha / 2)
-    right = rv.ppf(alpha / 2)
+    rv = chi2(df = n - 1)
+    left = rv.ppf(1 - alpha / 2) * disp_coef
+    right = rv.ppf(alpha / 2) * disp_coef
     
     return np.sqrt(nom / left), np.sqrt(nom / right)
