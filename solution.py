da0a3783aea833578588c541a7056f7bc258f18b
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-04 19:46:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,21 +3,18 @@ import numpy as np
 
 from scipy.stats import chi2
 
-
 chat_id = 260376781 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
     n = len(x)
-    s_squared = np.var(x, ddof=1)
-    disp_coef = 23 * (2 - np.pi / 2)
     
-    nom = (n - 1) * s_squared
-    rv = chi2(df = n - 1)
-    left = rv.ppf(1 - alpha / 2) * disp_coef
-    right = rv.ppf(alpha / 2) * disp_coef
+    x2 = np.array([xi**2 for xi in x])
+    x2_mean = x2.mean()
     
-    return np.sqrt(nom / left), np.sqrt(nom / right)
+    chi2_rv = chi2(df = 2 * n)
+    
+    left = chi2_rv.ppf(1 - alpha / 2)
+    right = chi2_rv.ppf(alpha / 2)
+    
+    return np.sqrt(n * x2_mean / (left * 23)), np.sqrt(n * x2_mean / (right * 23))
