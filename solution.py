f6d55450c917658ca04eb7cc252c5d6730852f0b
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-08 02:07:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,12 +9,12 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     n = len(x)
     
-    x2 = np.array([xi**2 for xi in x])
-    x2_mean = x2.mean()
+    x2 = [xi**2 for xi in x]
+    x2_sum = sum(x2)
     
-    chi2_rv = chi2(df = 2 * n)
+    chi2_rv = chi2(df=2 * n)
     
-    left = chi2_rv.ppf(1 - alpha / 2)
-    right = chi2_rv.ppf(alpha / 2)
+    left = chi2_rv.ppf(1 - alpha / 2) #b
+    right = chi2_rv.ppf(alpha / 2) #a
     
-    return np.sqrt(n * x2_mean / (left * 23)), np.sqrt(n * x2_mean / (right * 23))
+    return np.sqrt(x2_sum / (left * 23)), np.sqrt(x2_sum / (right * 23))
